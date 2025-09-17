6d88a3fdd30cc77d0121c6d0e6b487e95282c53e
Setha-idk <haksetha3@gmail.com>
2025-09-17 17:06:41 +0700

    add /home and /about to route and make pageController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PageController;
 Route::get('/', function () {
     return view('welcome');
 });
+ Route::get('/home', [PageController::class, 'home'])->name('home');
+ Route::get('/about', [PageController::class, 'about'])->name('about');
