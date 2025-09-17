d35f0a544e53e86c5be8891676c8f93b65ed230d
Setha-idk <haksetha3@gmail.com>
2025-09-17 17:31:58 +0700

    change home page from welcome to home

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/home', [PageController::class, 'home']);
+
+Route::get('/', [PageController::class, 'home']);
 Route::get('/about', [PageController::class, 'about']);
 
 Route::get('/user', [PageController::class, 'user']);
