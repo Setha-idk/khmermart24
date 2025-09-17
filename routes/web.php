174283b3d2fd58c2b28f76d06e9cf899dc58b0eb
Setha-idk <haksetha3@gmail.com>
2025-09-17 17:27:24 +0700

    add register page and user can register

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,9 @@ use App\Http\Controllers\PageController;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/home', [PageController::class, 'home'])->name('home');
-Route::get('/about', [PageController::class, 'about'])->name('about');
+Route::get('/home', [PageController::class, 'home']);
+Route::get('/about', [PageController::class, 'about']);
 
-Route::get('/user', [PageController::class, 'user'])->name('user');
+Route::get('/user', [PageController::class, 'user']);
+Route::get('/register', [PageController::class, 'register']);
+Route::post('/register', [PageController::class, 'store']);
